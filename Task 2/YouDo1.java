bb4b5f4e2e7d34c496df7b7d0546fe140b8b9769
Jibran Masum Didar <jmdidar9988@gmail.com>
2026-06-09 11:19:37 +0600

    Update YouDo1.java

--- Task 2/YouDo1.java
+++ Task 2/YouDo1.java
@@ -1,1 +1,21 @@
+/*
+You Do 01
+*/
 
+
+class ContestStates {
+    int[] scores = {85,90,95};
+    
+    void printFirstScore(){
+        System.out.println(scores[0]);
+        
+    }
+}
+
+public class Main {
+    public static void main (String[] args){
+        ContestStates contest= new ContestStates();
+        contest.printFirstScore();
+        
+    }
+}
