a6cddf0308d40e136dd2ab7e55db193178790e31
Jibran Masum Didar <jmdidar9988@gmail.com>
2026-06-23 11:19:57 +0600

    Update Main2.java

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,35 @@
+abstract class TeamMember {
+    protected String name;
+    
+    public TeamMember (String name) {
+        this.name = name;
+        
+    }
+    public String getName(){
+        return name;
+    }
+}
 
+interface BonusEligible {
+    void calculateBonus();
+}
+
+
+class StandardEmployee extends TeamMember implements BonusEligible {
+    public StandardEmployee(String name) {
+        super(name);
+    }
+    
+    @Override
+    public void calculateBonus(){
+        System.out.println("Calculating standard employee bonus for " + name +"...");
+    }
+}
+
+public class Main {
+    public static void main(String[] args ){
+        StandardEmployee employee = new StandardEmployee("Didar");
+        System.out.println("Employee Name " + employee.getName());
+        employee.calculateBonus();
+    }
+}
