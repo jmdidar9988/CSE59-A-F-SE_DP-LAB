aecee7230635ffb3272ca2fe2464c56a2f7661b9
Jibran Masum Didar <jmdidar9988@gmail.com>
2026-06-30 11:36:38 +0600

    Update Main1.java

--- Task 5/Main1.java
+++ Task 5/Main1.java
@@ -1,1 +1,50 @@
+interface Printer {
+    void printDocument();
+}
 
+interface Scanner {
+    void scanDocument();
+}
+
+interface FaxMachine {
+    void faxDocument();
+}
+
+class MultifunctionCopier implements Printer, Scanner, FaxMachine {
+    @Override
+    public void printDocument() {
+        System.out.println("MultiFunctionCopier: Printing document...");
+    }
+
+    @Override
+    public void scanDocument() {
+        System.out.println("MultiFunctionCopier: Scanning document...");
+    
+    }
+
+    @Override
+    public void faxDocument() {
+        System.out.println("MultiFunctionCopier: faxing document...");
+    }
+}
+
+class BasicPrinter implements Printer {
+    @Override
+    public void printDocument() {
+        System.out.println("BasicPrinter: Printing document...");
+    }
+}
+
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("Testing Basic Printer");
+        Printer basicPrinter = new BasicPrinter();
+        basicPrinter.printDocument();
+
+        System.out.println("\nTesting Multi-Function Copier");
+        MultifunctionCopier mfc = new MultifunctionCopier();
+        mfc.printDocument();
+        mfc.scanDocument();
+        mfc.faxDocument();
+    }
+}
